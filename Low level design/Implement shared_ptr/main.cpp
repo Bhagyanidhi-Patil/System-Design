161a28ee6c022eb92cb17314b4294ff475e5a512
Bhagyanidhi Patil <bhagyanidhi.patil2000@gmail.com>
2026-09-09 21:31:20 +0530

    added comments to shared ptr imp

--- Low level design/Implement shared_ptr/main.cpp
+++ Low level design/Implement shared_ptr/main.cpp
@@ -16,7 +16,7 @@ public:
 
 class sharedPtr {
     A* ptr;
-    int* refcount;
+    int* refcount;        //We keep refcount as a pointer because all copies of sharedPtr must share the same reference count.
 
 public:
     // Constructor
@@ -69,3 +69,74 @@ int main() {
 
     return 0;
 }
+
+
+/*
+Suppose refcount was just an integer
+class sharedPtr {
+    A* ptr;
+    int refcount;
+};
+
+When you do:
+
+sharedPtr p1(new A());  // refcount = 1
+sharedPtr p2 = p1;
+
+p2 gets its own copy of refcount.
+
+p1
+ ├── ptr ──────► A object
+ └── refcount = 1
+
+p2
+ ├── ptr ──────► Same A object
+ └── refcount = 2
+
+The counts are stored separately, so updating one doesn't update the other.
+
+----
+
+With int* refcount
+A* ptr;
+int* refcount;
+
+When p1 is created:
+
+ptr = new A();
+refcount = new int(1);
+
+Memory looks like:
+
+p1
+ ├── ptr ───────────► A object
+ │
+ └── refcount ──────► [ 1 ]
+
+Now when:
+
+sharedPtr p2 = p1;
+
+we do:
+
+ptr = other.ptr;
+refcount = other.refcount;
+
+(*refcount)++;
+
+Both objects now point to the same integer:
+
+p1 ──┐
+     ├──► A object
+p2 ──┘
+
+
+p1.refcount ──┐
+              ├──► [ 2 ]
+p2.refcount ──┘
+
+Now both p1 and p2 see the same reference count.
+
+
+
+*/
